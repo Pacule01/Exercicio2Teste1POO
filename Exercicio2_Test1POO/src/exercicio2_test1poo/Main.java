c84c9996bddb04505bac8d3306110bb96fe03221
Pacule01 <133216332+Pacule01@users.noreply.github.com>
2023-06-07 15:57:22 +0200

    Add files via upload
    Resolução do exercicio Nr 2 do teste_1 de POO1!

---
+++ Exercicio2_Test1POO/src/exercicio2_test1poo/Main.java
@@ -0,0 +1,11 @@
+package exercicio2_test1poo;
+
+// * @author Pacule, Manuel Arnaldo
+
+public class Main {
+
+    public static void main(String[] args) {
+      Menu m = new Menu();
+    }
+    
+}
